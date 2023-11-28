df3c5a67c6299110fdc1a3675c713c304311d9df
Matt Wiese <matthewwiese@users.noreply.github.com>
2023-11-28 09:58:19 -0500

    Account for GenomicsDBImport requiring a interval be specified

--- scripts/germline-disco.sh
+++ scripts/germline-disco.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+####################### NOTE ########################
+#   This is a general script for example purposes   #
+# Please modify it to fit your experimental design! #
+##################################################### 
+
 # Collated from Mohamed's pipeline scripts
 # Refer to GATK's docs: https://gatk.broadinstitute.org/hc/en-us/articles/360035535932-Germline-short-variant-discovery-SNPs-Indels
 # Arguments (positional):
@@ -44,8 +49,14 @@ do
     $GATK HaplotypeCaller -R $REFERENCE_FASTA -I ${SAMPLE_NAME}.dedup.bam -O ${SAMPLE_NAME}.gvcf.gz -ERC GVCF
 done
 
-VARIANT_ARGS=$(awk 'BEGIN{ORS=" "}; {print "-V " $0}' <(find . -type f -name "*.gvcf.gz"))
-$GATK GenomicsDBImport $VARIANT_ARGS --genomicsdb-workspace-path $GENOMICS_DB_PATH
+# NOTE: GenomicsDBImport requires an interval; in our case we will
+#       iterate through all contigs defined in the reference index (.fa.fai)
+#       and import them individually - you may not want this!
+for CONTIG in $(cut -f1 "${REFERENCE_FASTA}.fai")
+do
+    VARIANT_ARGS=$(awk 'BEGIN{ORS=" "}; {print "-V " $0}' <(find . -type f -name "*.gvcf.gz"))
+    $GATK GenomicsDBImport $VARIANT_ARGS --genomicsdb-workspace-path $GENOMICS_DB_PATH -L $CONTIG
+done
 
 for SAMPLE_FILE in "${@:3}"
 do
